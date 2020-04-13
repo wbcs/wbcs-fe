4b1c29c5cef98c0a19f8f78dd8e67b609a6214fd
wbcs <421768544@qq.com>
2020-04-13 23:11:22 +0800

    feat: 根据uid/gid打开聊天窗口,全局utils

---
+++ src/renderer/utils/chat.js
@@ -0,0 +1,28 @@
+import store from '@/store'
+import router from '@/router'
+
+export const openUser = (uid) => {
+  store.commit('NEW_CHAT', { uid })
+  router.replace({
+    path: '/app/chats',
+    query: {
+      uid
+    }
+  })
+}
+export const openGroup = (gid) => {
+  store.commit('NEW_CHAT', { gid })
+  router.replace({
+    path: '/app/chats',
+    query: {
+      gid
+    }
+  })
+}
+export const openChat = (gidOrUid) => {
+  if (gidOrUid[0] === 'u') {
+    openUser(gidOrUid)
+    return
+  }
+  openGroup(gidOrUid)
+}
