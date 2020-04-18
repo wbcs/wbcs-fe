b340beca1829e24151ff6b9f83911552cd0f6ed1
wbcs <421768544@qq.com>
2020-04-18 23:32:15 +0800

    feat: 数据请求抽离

---
+++ src/renderer/request.js
@@ -0,0 +1,15 @@
+import { remote } from 'electron'
+const SOCKET = remote.getGlobal('socket')
+
+export const fetchLogin = params =>
+  new Promise((resolve, reject) => {
+    SOCKET.emit('login', params, data => {
+      if (!data.isAllowLogin) return reject()
+      resolve(data)
+    })
+  })
+
+export const fetchAuthcode = params =>
+  new Promise((resolve) => {
+    SOCKET.emit('get-authcode', params, resolve)
+  })
