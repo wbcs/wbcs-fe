b7f70de90d9c8fd142d28441b7b8212706af8d1a
wbcs <421768544@qq.com>
2020-04-21 17:57:25 +0800

    chore: 支持多平台打包

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,27 @@
 const execa = require('execa')
-const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('./build-config')
+const { log } = require('./utils')
 
-const buildElectron = async () => {
+const buildElectron = async platform => {
   await execa(
     'electron-builder',
-    ['--mac', '--config', 'build.yml'],
+    [`--${platform}`, '--config', 'build.yml'],
     { stdio: 'inherit' }
   )
-  console.log(chalk.green('build electron success.'))
+  log.success('build electron success.')
 }
 
-const build = () => {
+const build = (platform = 'mac') => {
   webpack(config, err => {
     if (err) {
-      console.log(err)
+      log.error(err)
       return
     }
-    console.log(chalk.green('build web success.'))
-    buildElectron()
+    log.success('build web success.')
+    if (platform === 'web') return
+    buildElectron(platform)
   })
 }
 
-build()
+build(process.argv[2])
